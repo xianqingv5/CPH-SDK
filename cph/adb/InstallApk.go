56ed8a88d896354a80e6e0485e18ec25da76fde4
yeahmobi <yeahmobi@yeahmobideMacBook-Pro.local>
2022-07-06 17:23:00 +0800

    修改

--- cph/adb/InstallApk.go
+++ cph/adb/InstallApk.go
@@ -25,7 +25,7 @@ func GetPostBody(r *http.Request, format string) *AdbPostBody {
 		return nil
 	}
 
-	if (len(adb.ServerIds) == 0 && len(adb.PhoneIds) == 0) || len(adb.Content) == 0 {
+	if len(adb.PhoneIds) == 0 || len(adb.Content) == 0 {
 		return nil
 	}
 
@@ -34,19 +34,32 @@ func GetPostBody(r *http.Request, format string) *AdbPostBody {
 	return &adb
 }
 
+func GetInstallApkPostBody(r *http.Request) *AdbPostBody {
+	var adb AdbPostBody
+
+	err := json.NewDecoder(r.Body).Decode(&adb)
+	if err != nil {
+		return nil
+	}
+
+	if len(adb.PhoneIds) == 0 || len(adb.Content) == 0 {
+		return nil
+	}
+
+	return &adb
+}
+
 func InstallApk(w http.ResponseWriter, r *http.Request) {
 	r.Header.Add("content-type", "application/json")
 	resp := response2.NewResp()
 	uri := fmt.Sprintf("https://%s/v1/%s/cloud-phone/phones/commands", conf.Config.Huawei.Endpoint, conf.Config.Huawei.ProjectId)
-	//uri := "https://cph.cn-east-3.myhuaweicloud.com/v1/09402bad5e80f3902fc1c0188cab3cd5/cloud-phone/phones/commands"
-	postbody := GetPostBody(r, "install")
+	postbody := GetInstallApkPostBody(r)
 	if postbody == nil {
 		resp.BadReq(w)
 		return
 	}
 
 	d, _ := json.Marshal(postbody)
-
 	body, err := httphelper2.HttpPost(uri, d)
 	if err != nil {
 		log.Println("InstallApk err: ", err)
